8cd66a58dec96e2b993c6b40426ad73d99e0e1da
marcel-beetz <marcelbeetz91@gmail.com>
2014-12-03 12:30:27 +0100

    Zum Auslesen der Konfigurationsdatei

---
+++ Linux/config.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "config.h"
+
+#define BUFFR 512
+#define TOKEN "="
+
+
+/* Fehlt eventuell Fehlerbehandlung?? */
+
+// String umgebende Leerzeichen entfernen zum Vergleichen
+char *trim_space(char *str){
+	char *end;
+
+	// Führende Leerzeichen löschen
+	while(isspace(*str)){
+		str++;
+	}
+
+	// Alles Leerzeichen
+	if(*str == 0){ 
+		return str;
+	}
+
+	// Nachfolgende Leerzeichen löschen
+	end = str + strlen(str) - 1;
+	while(end > str && isspace(*end)){
+		end--;
+	}
+	
+	// Neue Nullterminierung hinzufügen
+	*(end+1) = 0;
+	return str;
+}
+
+
+// String umgebende Leerzeichen entfernen für Ausgabe
+size_t trim_space2(char *out, size_t len, const char *str){
+	if(len == 0){
+		return 0;
+	}
+
+	const char *end;
+	size_t out_size;
+
+	// Führende Leerzeichen löschen
+	while(isspace(*str)){
+		str++;
+	}
+	
+	// Alles Leerzeichen?
+	if(*str == 0){
+		*out = 0;
+		return 1;
+	}
+
+	// Nachfolgende Leerzeichen löschen
+	end = str + strlen(str) - 1;
+	while(end > str && isspace(*end)){
+		end--;
+	}
+	end++;
+
+	// Ausgabegröße setzen
+	out_size = (end - str) < len-1 ? (end - str) : len-1;
+
+	// Getrimmten String kopieren und neue Nullterminierung hinzufügen
+	memcpy(out, str, out_size);
+	out[out_size] = 0;
+	return out_size;
+}
+
+
+// Konfigurationsdaten von Konfigurationsdatei in configstruct einlesen
+struct config get_config(char *filename){
+	struct config configstruct;
+	FILE *file = fopen (filename, "r");
+	if (file != NULL){
+		char line[BUFFR];
+		while(fgets(line, sizeof(line), file) != NULL){
+			char *confvalue;
+			char *confitem;
+			confvalue = strtok(line, TOKEN);
+			while (confvalue != NULL) {
+				if (confitem == NULL) {
+					confitem = trim_space(confvalue);
+				} else {
+					if (strcmp(confitem, "hostname") == 0) {
+						trim_space2(configstruct.hostname, BUFFR, confvalue);
+					} else if (strcmp(confitem, "portnumber") == 0) {
+						trim_space2(configstruct.portnumber, BUFFR, confvalue);
+					} else if (strcmp(confitem, "gamekindname") == 0) {
+						trim_space2(configstruct.gamekindname, BUFFR, confvalue);
+					} else {
+						// Fehlerbehandlung hier sinnvoll?
+						perror("Fehler bei Auslesen der Konfigurationsdatei.\n");
+					}
+					confitem = NULL;
+				}
+				confvalue = strtok(NULL, TOKEN);
+			}
+		} 
+	} 
+	fclose(file);
+	return configstruct;
+}
