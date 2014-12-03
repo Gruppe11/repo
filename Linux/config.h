386f16f95fdf652c72e1216c3352df4684b9b515
marcel-beetz <marcelbeetz91@gmail.com>
2014-12-03 12:22:52 +0100

    Headerfile für config.c

---
+++ Linux/config.h
@@ -0,0 +1,18 @@
+#ifndef cnfig
+#define cnfig
+
+#define BUFFR 512
+
+// char[] Datentyp für portnumber sollte laut Aufgabenstellung anders sein
+struct config{
+	char hostname[BUFFR];
+	char portnumber[BUFFR];
+	char gamekindname[BUFFR];
+};
+
+
+struct config configstruct;
+struct config get_config(char *filename);
+
+
+#endif
