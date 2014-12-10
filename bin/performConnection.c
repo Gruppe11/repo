b6fea8a40e0149f8c4f20b87f7feb9c21e722b98
abauer1 <a.bauer1@campus.lmu.de>
2014-12-10 12:27:42 +0100

    Update performConnection.c
    Neue Funktion getSock. Ausgelagert aus der main-Methode führt diese Funktion nun den Verbindungsaufbau zum Server durch.

--- bin/performConnection.c
+++ bin/performConnection.c
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include "sharedmemory.h"
 #include "performConnection.h"
+#include "config.h"
 #define BUFFR 512
 #define MAXGAMENAME 50
 
@@ -33,7 +36,7 @@ void sendMessage(int sock, char* clientMessage) {
  * sock: Socket des Servers
  * serverMessage: Pointer auf String, in den die Nachricht geschrieben werden soll
  */
-void getLine(int sock, char* line2) {
+char* getLine(int sock) {
 
 	static char* line;
 	static char serverMessage[BUFFR];
@@ -51,8 +54,9 @@ void getLine(int sock, char* line2) {
 			bzero(buffer, BUFFR); // Fülle String mit Nullen
 			numToken = recv(sock, buffer, BUFFR, 0);
 
-				if (numToken < 0)
+				if (numToken < 0) {
 					perror("\nError reading from socket");
+				}
 
 			strcat(serverMessage, buffer);
 
@@ -61,8 +65,40 @@ void getLine(int sock, char* line2) {
 
 		line = strtok(serverMessage, "\n");
 	}
+	//printf("numToken: %i\n", numToken);
+	//printf("line: %s\n", line);
+	return line;
 
-	strcpy(line2, line);
+}
+
+int getSock(struct config configS) {
+	struct sockaddr_in host;
+	int sock;
+	
+	// Hostname in IP Adresse übersetzen
+	struct hostent* ip = gethostbyname(configS.hostname);
+	//printf("IP: %s\n", ip->h_name);
+	if (ip == NULL) {
+		perror("\nFehler beim Anfordern der IP");
+		return EXIT_FAILURE;
+	}
+
+	// Benötigte Variablen der Struktur Host Werte zuweisen
+	memcpy(&(host.sin_addr), ip->h_addr_list[0], ip->h_length);
+	host.sin_family = AF_INET;
+	host.sin_port = htons(configS.portnumber);
+	//printf("SIN_PORT: %i\n", host.sin_port);
+
+	// Verbindung aufbauen und überprüfen
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	//printf("SOCKET: %i\n", sock);
+	if (connect(sock, (struct sockaddr*) &host, sizeof(host)) == 0) {
+		printf("\nVerbindung hergestellt!\n");
+	} else {
+		perror("\nFehler beim Verbindungsaufbau");
+		return EXIT_FAILURE;
+	}
+	return sock;
 }
 
 /**
@@ -73,10 +109,9 @@ void getLine(int sock, char* line2) {
  * game_id: Game ID des Spiels
  * fd: Pipe für ???
  */
-int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm) {
+int performConnection(char* version, char* game_id, int fd[],shm * shm) {
 
 	char* line;
-	line = malloc(sizeof(char) * BUFFR);
 	char clientMessage[BUFFR];
 	char* errorMessage;
 
@@ -85,10 +120,13 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 	temp1 = malloc(sizeof(char) * 20);
 	int temp2;
 	int temp3;
+	int sock;
+	
+	sock = getSock(configstruct);
 
 	while (1) {
-		getLine(sock, line); // Empfange Nachricht von Server
-
+		line = getLine(sock); // Empfange Nachricht von Server
+		//printf("%s\n", line);
 		// Servernachricht verarbeiten
 		// Switch zwischen positiver/negativer Server Antwort
 		switch (line[0]) {
@@ -114,7 +152,6 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 			}
 */
 			if (strstr(line, "+ MNM Gameserver") != 0) {
-
 				// sende  Protocol Version
 				sprintf(clientMessage, "%s %s\n", "VERSION", version);
 				sendMessage(sock, clientMessage);
@@ -122,18 +159,14 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 				// lese Serverversion ein + gebe diese aus
 				sscanf(line, "%*s %*s %*s v%s", temp1);
 				printf("\nServer Version %s\n", temp1);
-
 			} else if (strstr(line, "+ Client version accepted") != 0) {
-
 				// sende Game ID
 				sprintf(clientMessage, "%s %s\n", "ID", game_id);
 				sendMessage(sock, clientMessage);
 
 				// gebe Info aus
 				printf("Client Version %s akzeptiert\n", version);
-
 			} else if (strstr(line, "+ PLAYING") != 0) {
-
 				// sende Player
 				sprintf(clientMessage, "%s\n", "PLAYER");
 				sendMessage(sock, clientMessage);
@@ -146,15 +179,13 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 				}
 				printf("Spieltyp %s akzeptiert\n\n", temp1);
 
-				getLine(sock, line); // nächste Zeile
+				line = getLine(sock); // nächste Zeile "+ <<Game-Name>>"
 
 				// lese Spielname ein + gebe diesen aus
 				sscanf(line, "%*s %s", temp1);
 				printf("Spielname: %s\n", temp1);
 				strncpy(shm->spielname,temp1,sizeof(shm->spielname) );
-				
 			} else if (strstr(line, "+ YOU") != 0) {
-
 				// lese eigene Spielervariablen ein + gebe diese aus
 				sscanf(line, "%*s %*s %d %[^\n]s", &temp2, temp1);
 				printf("Du (%s) bist Spieler #%d\n", temp1, temp2 + 1);			
@@ -163,10 +194,10 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 				shm->spieleratt[shm->eigspielernummer-1].spielernummer = shm->eigspielernummer;	
 				strncpy(shm->spieleratt[shm->eigspielernummer-1].spielername,temp1,sizeof(shm->spieleratt[shm->eigspielernummer-1].spielername));	
 				shm->spieleratt[shm->eigspielernummer-1].regflag = 1;
-				getLine(sock, line); // nächste Zeile
+				line = getLine(sock); // nächste Zeile "+ TOTAL 2 <<Spieleranzahl>>"
 				sscanf(line, "%*s %*s %d", &temp2 );
 				shm->anzahlspieler = temp2;
-				getLine(sock, line); // nächste Zeile
+				line = getLine(sock); // nächste Zeile "+ <<Spielernummer>> <<Spielername>> <<Bereit>>"
 
 				// lese Gegner Spielervariablen ein + gebe diese aus
 				sscanf(line, "%*s %d", &temp2);
@@ -210,6 +241,7 @@ int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm
 		default:
 
 			fprintf(stderr, "\nServernachricht kann nicht verarbeitet werden\n");
+			fprintf(stdout, "%s\n", line);
 			return EXIT_FAILURE;
 		}
 	}
