bf84ca1de49b76a9ba2ec45ce4085c19970c48a6
abauer1 <a.bauer1@campus.lmu.de>
2014-12-10 12:28:37 +0100

    Update performConnection.h
    Funktion performConnection angepasst.

--- bin/performConnection.h
+++ bin/performConnection.h
@@ -3,6 +3,6 @@
 
 void sendMessage(int sock, char* clientMessage);
 void getLine(int sock, char* line);
-int performConnection(int sock, char* version, char* game_id, int fd[],shm * shm);
+int performConnection(char* version, char* game_id, int fd[],shm * shm);
 
 #endif
